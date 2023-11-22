07dc90b46fff240c08c2136518d83502404e23c6
erentsen <erecenmancaev@gmail.com>
2023-11-23 02:26:37 +0300

    add solution task add-Length

--- 8kyu/add-Length.js
+++ 8kyu/add-Length.js
@@ -8,4 +8,8 @@
 // Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
 
 // Note: String will have at least one element; words will always be separated by a space.
+//✅SOLUTION:
 
+function addLength(str){
+  return str.split(" ").map( word => `${word} ${word.length}`)
+}
