0ba8fc10154b9ab5967276f1d37f130f68519d36
erentsen <erecenmancaev@gmail.com>
2023-11-25 16:31:00 +0300

    add solution task mumbling

--- 7kyu/mumbling.js
+++ 7kyu/mumbling.js
@@ -8,3 +8,15 @@ accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt") -> "C-Ww-Aaa-Tttt"
 The parameter of accum is a string which includes only letters from a..z and A..Z.
 */
+//✅SOLUTION:
+function accum(s) {
+  let res = [];
+  for(var i = 0; i < str.length; i++) {
+    let row = '';
+    for(var j = 0; j < i + 1; j++) {
+       row += j===0 ? str[i].toUpperCase() : str[i].toLowerCase();      
+    }
+    res.push(row);
+  }
+  return res.join('-');
+}
